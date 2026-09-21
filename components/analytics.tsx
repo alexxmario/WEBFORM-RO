822ceb57d977258cbd3fdae334f31deefbef3162
Alex Ionescu <alexmario@Alexs-MacBook-Air-3.local>
2026-09-21 15:14:24 +0300

    Add Google Ads tag

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -5,25 +5,24 @@ import Script from "next/script";
 export function AnalyticsScripts() {
   const gaId = process.env.NEXT_PUBLIC_GA_ID;
   const fathomId = process.env.NEXT_PUBLIC_FATHOM_ID;
+  const googleAdsId = "AW-18465571325";
+  const googleTagId = gaId || googleAdsId;
 
   return (
     <>
-      {gaId ? (
-        <>
-          <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
-            strategy="afterInteractive"
-          />
-          <Script id="ga-setup" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${gaId}');
-            `}
-          </Script>
-        </>
-      ) : null}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${googleTagId}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-tag-setup" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          ${gaId ? `gtag('config', '${gaId}');` : ""}
+          gtag('config', '${googleAdsId}');
+        `}
+      </Script>
       {fathomId ? (
         <Script
           src="https://cdn.usefathom.com/script.js"
